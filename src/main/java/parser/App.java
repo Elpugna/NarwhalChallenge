dbea0e743160003ef82d1dfb5901ebf63b047176
elPugna <tomi.pugna@gmail.com>
2023-12-18 20:29:23 +0100

    Documenting functions

--- src/main/java/parser/App.java
+++ src/main/java/parser/App.java
@@ -17,6 +17,12 @@ public class App {
         }
     }
 
+    /**
+     * The parseFile Function parses the file and reads the size and type ob boxes on it.
+     * @param stream - the stream of type FileInputStream
+     * @param depth - the depth of the boxes
+     * @throws IOException - if the stream has been closed. See FileInputStream.available() reference.
+     */
     private static void parseFile(FileInputStream stream, int depth) throws IOException{
         while (stream.available() > 0) {
             int boxSize = readInt(stream);
@@ -34,6 +40,13 @@ public class App {
     }
 
 
+    /**
+     * Reads 4 bytes of the stream that determines the box size and returns it
+     *
+     * @param stream - the stream of bytes of the mpeg file
+     * @return - int representing the box size
+     * @throws IOException
+     */
     private static int readInt(FileInputStream stream) throws IOException {
         byte[] bytes = new byte[4];
         stream.read(bytes);
@@ -41,12 +54,23 @@ public class App {
         return bytes[0] << 24 | (bytes[1] & 0xff) << 16 | (bytes[2] & 0xff) << 8 | (bytes[3] & 0xff);
     }
 
+    /**
+     * Readt 4 bytes of the stream that determines the box type and returns it
+     * @param stream - the stream of bytes of the mpeg file
+     * @return - String representing the box type
+     * @throws IOException
+     */
     private static String readBoxType(FileInputStream stream) throws IOException {
         byte[] bytes = new byte[4];
         stream.read(bytes);
         return new String(bytes);
     }
 
+    /**
+     * This function prints to console the content of the mdat box.
+     * @param stream - stream of bytes of the file.
+     * @throws IOException
+     */
     private static void parseMdatBox(FileInputStream stream) throws IOException {
         byte[] mdatContent = new byte[stream.available()];
         stream.read(mdatContent);
