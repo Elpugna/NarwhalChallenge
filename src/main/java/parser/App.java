571742fe81ad3aa7dbbb6098caeae827c3aacde0
elPugna <tomi.pugna@gmail.com>
2023-12-20 22:51:10 +0100

    Adding Base64 image decodification

--- src/main/java/parser/App.java
+++ src/main/java/parser/App.java
@@ -1,6 +1,12 @@
 package parser;
 
 import java.io.*;
+import java.util.Base64;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App {
     //Modify this value to match the file when executing (Path from project's root)
@@ -10,7 +16,7 @@ public class App {
         File inputFile = new File(filePath);
         try (FileInputStream fis = new FileInputStream(inputFile)) {
             parseFile(fis, 0);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -21,7 +27,7 @@ public class App {
      * @param depth - the depth of the boxes
      * @throws IOException - if the stream has been closed. See FileInputStream.available() reference.
      */
-    private static void parseFile(FileInputStream stream, int depth) throws IOException {
+    private static void parseFile(FileInputStream stream, int depth) throws IOException, InterruptedException{
         while (stream.available() > 0) {
             int boxSize = readInt(stream);
             String boxType = readBoxType(stream);
@@ -68,15 +74,38 @@ public class App {
 
     /**
      * This method prints the content of the MDAT box to the console.
+     * It also decodes the Base64 images on it and generates the files for them.
      * @param stream - the byte stream of the file
      * @throws IOException
      */
-    private static void parseMdatBox(InputStream stream) throws IOException {
+    private static void parseMdatBox(InputStream stream) throws IOException, InterruptedException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
         String line;
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        StringBuilder currentImage = new StringBuilder();
         System.out.println("Mdat content:");
+        Pattern imageNamePattern = Pattern.compile("id=\"[^\"]*\"");
+        Pattern imageTypePattern = Pattern.compile("imagetype=\"[^\"]*\"");
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
+            if (line.contains("<smpte:image")) {
+                Matcher nameMatcher = imageNamePattern.matcher(line);
+                Matcher typeMatcher = imageTypePattern.matcher(line);
+                String imageName = "", imageType = "";
+                while (nameMatcher.find() && typeMatcher.find()) {
+                    imageName = nameMatcher.group().substring(4, nameMatcher.group().length()-1);
+                     imageType = typeMatcher.group().substring(11, typeMatcher.group().length()-1).toLowerCase();
+                }
+                line = reader.readLine();
+                System.out.println(line);
+                byte[] imageBytes = Base64.getDecoder().decode(line);
+                try (OutputStream os = new FileOutputStream(imageName + "."+ imageType)) {
+                    os.write(imageBytes);
+                }
+                currentImage.setLength(0);
+            }
         }
+        executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
     }
 }
